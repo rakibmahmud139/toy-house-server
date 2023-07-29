18f7d0142bd8237ee52e86508ab29efb6a6967d7
Rakibmahmud <hasansaikat74@gmail.com>
2023-07-29 12:59:28 +0600

    added vercel config

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
